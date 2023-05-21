a5b0779e420f8ed3734cfbc4509d88ec4d64a51b
Marta Stefaniak <vocado7@gmail.com>
2023-05-21 10:16:39 +0200

    adds basic configuration for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router.tsx';
 
 function App() {
     return (
         <>
+            <RouterProvider router={router} />
             <h1>Alegrosz</h1>
         </>
     );
